5d9f418f9f321886ba473b974fccfce590e503ca
kaitokikuchi <kyetokikuchi@gmail.com>
2021-03-31 14:41:34 -0700

    fibonacci

--- Algorithmic Toolbox/Algorithmic_Toolbox_Assignments/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ Algorithmic Toolbox/Algorithmic_Toolbox_Assignments/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,9 @@
 # Uses python3
-def calc_fib(n):
-    if (n <= 1):
-        return n
-
-    return calc_fib(n - 1) + calc_fib(n - 2)
+def fibonacci(n):
+    fibarray = [0,1]
+    for i in range(2,n+1):
+        fibarray.append(fibarray[i-2]+fibarray[i-1])
+    return fibarray[n]
 
 n = int(input())
-print(calc_fib(n))
+print(fibonacci(n))
