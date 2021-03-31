06747aa45990e7ff24263ffcd5d58a6f34e3c081
kaitokikuchi <kyetokikuchi@gmail.com>
2021-03-31 16:43:32 -0700

    lcm

--- Algorithmic Toolbox/Algorithmic_Toolbox_Assignments/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ Algorithmic Toolbox/Algorithmic_Toolbox_Assignments/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,15 +1,20 @@
 # Uses python3
 import sys
 
-def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
+def euclidGCD(a,b):
+  if b == 0:
+    return a
+  else:
+    c = a%b
+    return euclidGCD(b,c)
 
-    return a*b
+def lcm(a, b):
+    gcd = euclidGCD(a, b)
+    lcm = a*b/gcd
+    return int(lcm)
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    #input = sys.stdin.read()
+    a, b = map(int, input().split())
+    print(lcm(a, b))
 
