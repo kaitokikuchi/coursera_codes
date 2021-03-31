e60d848daf6d23fb4de4da322ccc9dbc16b6cd3f
kaitokikuchi <kyetokikuchi@gmail.com>
2021-03-31 16:04:03 -0700

    optimized fibonacci last digit function to store only the last digit to conserve memory

--- Algorithmic Toolbox/Algorithmic_Toolbox_Assignments/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ Algorithmic Toolbox/Algorithmic_Toolbox_Assignments/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -1,13 +1,13 @@
 # Uses python3
 import sys
 
-def get_fibonacci_last_digit(n):
+def fibonacci_ld(n):
     fibarray = [0,1]
     for i in range(2,n+1):
-        fibarray.append(fibarray[i-2]+fibarray[i-1])
-    return fibarray[n]%10
+        fibarray.append((fibarray[i-2]+fibarray[i-1])%10)
+    return fibarray[n]
 
 if __name__ == '__main__':
     #input = sys.stdin.read()
     n = int(input())
-    print(get_fibonacci_last_digit(n))
+    print(get_fibonacci_ld(n))
